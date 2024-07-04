791f04e3da0f433f9b7b052364f992a00263214e
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-04 21:04:24 +0100

    Python Annote

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Annotated function that adds to float a and b
+"""
+
+def add(a: float, b: float) -> float:
+    """ Sums up two flaots"""
+    return (a+ b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Annotated function floor that returns floor 
+of float"""
+
+def floor(n: float) -> int:
+    """Returns floor of float"""
+    return int(n)
