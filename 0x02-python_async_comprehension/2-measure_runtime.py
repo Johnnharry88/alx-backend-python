50c7c47f9c63ba128fe6241d9652dfce33cf86bc
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-09 12:56:59 +0100

    Python Backend

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -14,5 +14,5 @@ async def measure_runtime() -> float:
     s_time = time.time()
     await asyncio.gather(*(async_comp() for x in range(4)))
     e_time = time.time()
-    time_interval = e_time = s_time
+    time_interval = e_time - s_time
     return time_interval
